c0c4985931d4af9541a38ce98ba4fe58904dd4e8
SenorgCC <Alex.Sinicyn@gmx.de>
2017-02-22 15:18:44 +0100

    Version 1

---
+++ Server.py
@@ -0,0 +1,22 @@
+import socket
+import json
+
+#@192.168.0.1
+s= socket.socket()
+
+host = '192.168.0.23'
+port = 12345
+
+s.bind((host, port))
+
+s.listen(5)
+
+msg = 'Thank you for connecting'
+
+while True:
+    c, addr = s.accept()
+    print('Got connection from ', addr[0])
+    data = json.loads(c.recv(4096).decode('utf-8'))
+    print('Hostname: ', data["HostName"], 'Wert: ', data["Wert"], 'Grenzwert: ', data["Grenzwert"])
+    #s.sendto("1".encode('utf-8'), (host, port))
+    c.close()
