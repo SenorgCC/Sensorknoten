5a090bff1ca96b4dcee95d0352a5910e6ffd528a
SenorgCC <Alex.Sinicyn@gmx.de>
2017-02-27 16:25:07 +0100

    # StartSensor created

---
+++ StartSensor.py
@@ -0,0 +1,10 @@
+from Sensor import Sensor
+# Dieses Programm dient zum Initialisieren und Steuern von Sensoren
+
+
+# Initialisierung des Sensors
+sensor1 = Sensor(3, 0, 18)
+
+#Ausführen des Sensors
+sensor1.flammensensor()
+
