2f482cbf62b93bf3888a09c2e86d24bad7126e7d
SenorgCC <Alex.Sinicyn@gmx.de>
2017-03-03 14:11:43 +0100

    Sensorcontroller implemented

--- StartSensor.py
+++ StartSensor.py
@@ -1,12 +1,63 @@
 from sensor import Sensor
+import time
 # Dieses Programm dient zum Initialisieren und Steuern von Sensoren
 
 
 # Initialisierung des Sensors
-sensor1 = Sensor(3, 0, 18)
+# Sensor(SEN_ID, Analog_PIN, Digital_PIN)
+# Zuweisung der Analoganschluesse fuer den ADWandler
 
-#Ausfuehren des Sensors
-sensor1.flammensensor()
+A0 = 0x40
+A1 = 0x41
+A2 = 0xA2
+A3 = 0xA3
+# Inialisierung der Sensoren
+# DHT11 - Luftfeuchtigkeit und Temperatur
+sensor1 = Sensor(1, None, 26)
+sensor2 = Sensor(2, None, 26)
 
+# Init Mikrofon KY-038
+sensor3 = Sensor(3, A0, 18)
+# Lichtschranke KY-010
+sensor4 = Sensor(4, None, 24)
+# Flammensensor KY-026
+sensor5 = Sensor(5, A1, 17)
+# Lichtsensor KY-018
+sensor6 = Sensor(6, A2, None)
+# Schocksensor TAP-Module
+sensor7 = Sensor(7, None, 5)
 
+# Initialisierung eventgesteuerte Sensoren
+sensor3.add_event_mikrofon()
+sensor4.lichtschranke()
+sensor5.add_event_flammensensor()
+sensor7.schocksensor()
 
+counter = 0
+
+try:
+    while True:
+        sensor5.flammensensor()
+        sensor3.mikrofon()
+        if counter == 2:
+            sensor6.lichtsensor()
+
+        if counter == 4:
+            sensor1.temperatur()
+            sensor2.luftfeuchtigkeit()
+            counter = 0
+
+        counter += 1
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    print("KeyBoard Interrupt!")
+    print("Cleaning up GPIO")
+    sensor1.cleanup()
+    sensor2.cleanup()
+    sensor3.cleanup()
+    sensor4.cleanup()
+    sensor5.cleanup()
+    sensor6.cleanup()
+    sensor7.cleanup()
+    print("Programm terminated!")
