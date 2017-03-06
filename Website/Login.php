43e0884a548cbd6b019d2b1eff44787f5861ef97
Harm86 <harmischweizer@googlemail.com>
2017-03-06 13:19:49 +0100

    Login entworfen

--- Website/Login.php
+++ Website/Login.php
@@ -1,3 +1,5 @@
+<?php include("scripts/loginscript.php"); ?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -9,14 +11,19 @@
 </head>
 <body>
 
+<?php
+if(isset($errorMessage)) {
+    echo $errorMessage;
+}
+?>
+
 <div class="container">
-    <form class="form-signin">
+    <form class="form-signin" method="post" action="?login=1">
         <h2 class="form-signin-heading">Sensorüberwachung</h2>
         <label class="sr-only">Name</label>
-        <input type="name" id="inputname" class="form-control" placeholder="Name" autofocus>
+        <input type="name" name="username" id="inputname" class="form-control" placeholder="Name" autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control"
-               placeholder="Password">
+        <input type="password" name="passwort" id="inputPassword" class="form-control" placeholder="Password">
         <button id="submitbtn" class="btn btn-lg btn-primary btn-block" type="submit">
             Sign in
         </button>
--- Website/Uebersicht.php
+++ Website/Uebersicht.php
@@ -1,16 +1,30 @@
+<?php
+session_start();
+if(!isset($_SESSION['userid'])) {
+    die('Bitte zuerst <a href="login.php">einloggen</a>');
+}
+
+//Abfrage der Nutzer ID vom Login
+$userid = $_SESSION['userid'];
+
+echo "Hallo User: ".$userid;
+?>
+
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
     <meta charset="UTF-8">
     <title>Übersicht</title>
     <link rel="stylesheet" href="sources/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="css/übersicht.css">
-    <script src="sources/bootstrap/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="css/uebersicht.css">
     <script src="sources/jquery-3.1.1.min.js"></script>
-    <script src="scripts/Übersicht.js"></script>
+    <script src="sources/bootstrap/js/bootstrap.js"></script>
+    <script src="scripts/uebersichtscript.js"></script>
 
 </head>
-<body onload="javascript:start()">
+<body>
+
 <!-- Beginn Navbar-->
 <nav class="navbar navbar-default" id="navbar">
     <div class="container-fluid">
