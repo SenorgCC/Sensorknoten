9e79e774e433313eaa2985c0f5317346e8bd615f
Jan <Jan.Mannherz@online.de>
2017-03-06 16:53:54 +0100

    - Tabellen werden mit PHP erstellt. - Messwerte in Tablle eintragen muss noch gemacht werden

--- Website/Uebersicht.php
+++ Website/Uebersicht.php
@@ -1,13 +1,14 @@
 <?php
-session_start();
-if(!isset($_SESSION['userid'])) {
-    die('Bitte zuerst <a href="login.php">einloggen</a>');
-}
+//session_start();
+//if(!isset($_SESSION['userid'])) {
+//    die('Bitte zuerst <a href="login.php">einloggen</a>');
+//}
 
 //Abfrage der Nutzer ID vom Login
-$userid = $_SESSION['userid'];
+//$userid = $_SESSION['userid'];
 
-echo "Hallo User: ".$userid;
+//echo "Hallo User: ".$userid;
+
 ?>
 
 
@@ -30,7 +31,8 @@ echo "Hallo User: ".$userid;
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -54,43 +56,60 @@ echo "Hallo User: ".$userid;
     </div><!-- /.container-fluid -->
 </nav>
 <!-- Ende Navbar-->
-<h2 class="sub-header"> Sensor 01 </h2>
-<div class = "table-responsive">
-    <table class = "table table-striped">
-        <thead>
-        <tr>
-            <th>Temperatur</th>
-            <th>Luftfeuchtigkeit</th>
-            <th>Flammsensor</th>
-            <th>Lichtschranke</th>
-            <th>Mikrofon</th>
-            <th>Lichtsensor</th>
-            <th>Schocksensor</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <td>26 °</td>
-            <td>5 %</td>
-            <td>AUS</td>
-            <td>AUS</td>
-            <td>AUS</td>
-            <td>AUS</td>
-            <td>AN</td>
-        </tr>
-        </tbody>
-    </table>
-</div>
 <?php
 @$mysqli = new mysqli('10.35.249.48', 'k35630_test', 'test1234', 'k35630_testdata');
 if ($mysqli->connect_errno) {
     echo 'Sorry, die Verbindung zu unserem superfetten endgeilen 
-        Server ist hops gegangen. Wegen '.$mysqli -> connect_error;
+        Server ist hops gegangen. Wegen ' . $mysqli->connect_error;
 }
-$query = "SELECT KN_ID FROM Sensorknoten";
-$result = $mysqli -> query ($query);
+$query = "SELECT Knotennamen FROM Sensorknoten";
+$check= "";
+$result = $mysqli->query($query);
 while ($row = $result->fetch_assoc()) {
-    print_r("HALLO");
+    $check = $row['Knotennamen'];
+    echo "<h2 class=\"sub-header\"> " . $row['Knotennamen'] . "</h2>";
+    echo "<div class=\"table-responsive\">";
+    echo "    <table class=\"table table-striped\">";
+    echo "        <thead>";
+    echo "        <tr>";
+    echo "            <th>Temperatur</th>";
+    echo "            <th>Luftfeuchtigkeit</th>";
+    echo "            <th>Flammsensor</th>";
+    echo "            <th>Lichtschranke</th>";
+    echo "            <th>Mikrofon</th>";
+    echo "            <th>Lichtsensor</th>";
+    echo "            <th>Schocksensor</th>";
+    echo "        </tr>";
+    echo "        </thead>";
+    echo "        <tbody>";
+    echo "        <tr>";
+    echo "            <td>26 °</td>";
+    echo "            <td>5 %</td>";
+    echo "            <td>AUS</td>";
+    echo "            <td>AUS</td>";
+    echo "            <td>AUS</td>";
+    echo "            <td>AUS</td>";
+    echo "            <td>AN</td>";
+    echo "        </tr>";
+    echo "        </tbody>";
+    echo "    </table>";
+    echo "</div>";
+}
+$select = "SELECT *
+FROM(
+    SELECT Messwert, Timestamp, M . SEN_ID, Sensorname FROM Sensorknoten SK
+        INNER JOIN Sensorknoten_Messwerte AS SM ON(SK . KN_ID = SM . KN_ID)
+         INNER JOIN Messwerte AS M ON(SM . MESS_ID = M . MESS_ID)
+    INNER JOIN Sensoren S ON(M . SEN_ID = S . SEN_ID) 
+    WHERE Sensorname = $check
+    ORDER BY Timestamp desc
+) AS sub
+GROUP BY Sensorname
+ORDER BY SEN_ID";
+$result2 = $mysqli->query($select);
+    //print_r($result2);
+while ($row2 = $result2->fetch_assoc()) {
+    print_r($row2);
 }
 ?>
 </body>
