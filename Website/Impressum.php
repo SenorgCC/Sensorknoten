7c6aeb31b673f962cd9a35b7e9619e98455e4750
Harm86 <harmischweizer@googlemail.com>
2017-03-08 12:13:54 +0100

    Überpüfung in Impressum eingebaut

--- Website/Impressum.php
+++ Website/Impressum.php
@@ -1,3 +1,16 @@
+<?php
+/**session_start();
+if(!isset($_SESSION['userid'])) {
+   die('Bitte zuerst <a href="login.php">einloggen</a>');
+}
+
+//Abfrage der Nutzer ID vom Login
+$userid = $_SESSION['userid'];
+
+echo "Hallo User: ".$userid;
+*/
+?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
