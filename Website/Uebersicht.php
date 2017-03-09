cbf7a3024f6046fc1e8401770931585757f41955
Harm86 <harmischweizer@googlemail.com>
2017-03-09 13:53:04 +0100

    Navbar angepasst, statistik und kamera integriert

--- Website/Uebersicht.php
+++ Website/Uebersicht.php
@@ -47,6 +47,8 @@ echo "Hallo User: ".$userid;
                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                    <ul class="nav navbar-nav">
                        <li class="active"><a href="Uebersicht.php">Übersicht <span class="sr-only">(current)</span></a></li>
+                       <li><a href="Statistikenuebersicht.php">Statistik</a></li>
+                       <li><a href="Kamera.php">Webcam</a> </li>
                    </ul>
                    <!-- Rechte Seite der Navbar -->
                    <ul class="nav navbar-nav navbar-right">
