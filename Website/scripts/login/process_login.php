7f2ca6e6524f3491c02de990b3798a5d8f575718
Harm <harmischweizer@googlemail.com>
2017-03-10 19:42:15 +0100

    Register.php an unseren Stil angepasst

--- Website/scripts/login/process_login.php
+++ Website/scripts/login/process_login.php
@@ -13,7 +13,7 @@ if (isset($_POST['email'], $_POST['p'])) {
         header('Location: ../protected_page.php');
     } else {
         // Login fehlgeschlagen
-        header('Location: ../index.php?error=1');
+        header('Location: ../Login.php?error=1');
     }
 } else {
     // Die korrekten POST-Variablen wurden nicht zu dieser Seite geschickt.
