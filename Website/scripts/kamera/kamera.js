2cbc85abd94d31013fabf2dbada5cf72588faf7b
SenorgCC <Alex.Sinicyn@gmx.de>
2017-03-14 10:21:14 +0100

    Webcamconfiguration added

--- Website/scripts/kamera/kamera.js
+++ Website/scripts/kamera/kamera.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
     //var daten=[];
 
     $.post('scripts/kamera/kamera.php', function (data) {
-
         data = JSON.parse(data);
         $.each(data, function (index, value) {
             if (value != 0) {
@@ -10,11 +9,22 @@ $(document).ready(function () {
             }
         });
     });
+
+    $.post('scripts/kamera/sensorknotenauswahl.php', function (data) {
+        data = JSON.parse(data);
+        $.each(data, function (index, value) {
+            if (value != 0) {
+                $('#Sensorknotenauswahl').append('<option>' + value + '</option>');
+            }
+        });
+    });
+
     $('#kameraBtn').click(function () {
         var Sensorknoten = $('#kamera option:selected').text();
         var sendedata = {
             sensorknoten: Sensorknoten
         };
+
         $.post('scripts/kamera/kameraadresse.php', sendedata, function (data) {
             //Der Post liefert ein Array, an dem die erste stelle null ist, in der 2. ist der echte Wert
             data = JSON.parse(data);
@@ -25,6 +35,18 @@ $(document).ready(function () {
                 '</iframe>'
             );
         });
+    });
+    $('#speichernBtn').click(function(){
+        var Sensorknoten = $('#Sensorknotenauswahl option:selected').text();
+        var kameramodus = $('input[name=optradio]:checked','#auswahl').val();
+        var sendedata = {
+            sensorknoten: Sensorknoten,
+            mode: kameramodus
+        };
+        $.post('scripts/kamera/kameraconfig.php', sendedata, function(data){
+            location.reload();
+        });
+
     });
 
     //
---
+++ Website/scripts/kamera/kameraconfig.php
@@ -0,0 +1,15 @@
+<?php
+$sensorknoten = $_POST['name'];
+$kameramodus = $_POST['mode'];
+@$mysqli = new mysqli('localhost', 'root', 'Piroot', 'Sicherheitssystem');
+if ($mysqli->connect_errno) {
+    echo 'Sorry, die Verbindung zu unserem superfetten endgeilen
+        Server ist hops gegangen. Wegen ' . $mysqli->connect_error;
+}
+
+$query = "UPDATE Sensorknoten SET Webcam = '".$kameramodus."' WHERE Knotennamen = '".$sensorknoten."' ";
+$result = $mysqli->query($query);
+
+echo json_encode($result);
+$mysqli->close();
+?>
