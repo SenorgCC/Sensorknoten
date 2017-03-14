eb6a47536ba04cfd43d14a74aff188c7b3a5ef97
Jan <Jan.Mannherz@online.de>
2017-03-14 15:08:35 +0100

    Refactoring

--- Website/scripts/kamera/kameraconfig.php
+++ Website/scripts/kamera/kameraconfig.php
@@ -1,4 +1,5 @@
 <?php
+// Ändert den Datenbankeintrag der Webcam 1 | 0
 $sensorknoten = $_POST['sensorknoten'];
 $kameramodus = $_POST['mode'];
 @$mysqli = new mysqli('localhost', 'root', 'Piroot', 'Sicherheitssystem');
@@ -6,10 +7,8 @@ if ($mysqli->connect_errno) {
     echo 'Sorry, die Verbindung zu unserem superfetten endgeilen
         Server ist hops gegangen. Wegen ' . $mysqli->connect_error;
 }
-
 $query = "UPDATE Sensorknoten SET Webcam = '".$kameramodus."' WHERE Knotennamen = '".$sensorknoten."' ";
 $result = $mysqli->query($query);
-
 echo json_encode($result);
 $mysqli->close();
 ?>
