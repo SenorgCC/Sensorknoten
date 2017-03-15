9ef115126e371cd8ae02f490c608827413c8347c
SenorgCC <Alex.Sinicyn@gmx.de>
2017-03-15 09:35:04 +0100

    Formatiert

--- Server.py
+++ Server.py
@@ -5,45 +5,43 @@ import json
 import MySQLdb as mdb
 import sys
 
-s= socket.socket()
+s = socket.socket()
 host = '192.168.178.1'
 port = 12345
 s.bind((host, port))
 s.listen(5)
 msg = 'Thank you for connecting'
 while True:
-        c, addr = s.accept()
-        print('Got connection from ', addr)
-        data = json.loads(c.recv(4096).encode('utf-8'))
-        Name = str(data["Name"])
-        SEN_ID = str(data["SEN_ID"])
-        Wert = str(data["Messwert"])
-	adresse = str(addr[0])
-	try:
-	        con = mdb.connect('localhost', 'root', 'Piroot', 'Sicherheitssystem')
-      		print("SQL-Connection successful")
-	except _mysql.Error, e:
-		print ("SQL-Connection failed. Error %d: %s" %(e.args[0], e.args[1]))
-	with con:
-		cur = con.cursor()
-		row_count = cur.execute("SELECT KN_ID FROM Sensorknoten WHERE Knotennamen = %s", (Name, ))
-		# Wenn ein Datenbankeintrag gefunden wird, wird die IPv4_Adresse erneuert
-		if row_count == 1:
-			KN_ID = cur.fetchone()
-			cur.execute("""UPDATE Sensorknoten SET IPv4_Adresse = %s WHERE Knotennamen = %s""", (adresse, Name))
-			cur.execute("""INSERT INTO Messwerte (SEN_ID, Messwert)VALUES(%s, %s)""", (SEN_ID, Wert))
-			# Die zuletzt erstellte ID wird übergeben
-			MessID = cur.insert_id()
-			cur.execute("""INSERT INTO Sensorknoten_Messwerte (KN_ID, MESS_ID)VALUES(%s, %s)""", (KN_ID, MessID))
-		# Wenn kein Datenbankeintrag gefunden wird, wird ein neuer Datenbankeintrag erstellt
-		elif row_count == 0:
-			cur.execute("""INSERT INTO Sensorknoten (Knotennamen, IPv4_Adresse)VALUES(%s, %s)""", (Name, adresse))
-			# Die zuletzt erstellte ID wird übergeben
-			KN_ID = cur.insert_id()
-			cur.execute("""INSERT INTO Messwerte (SEN_ID, Messwert)VALUES(%s, %s)""", (SEN_ID, Wert))
-			# Die zuletzt erstellte ID wird übergeben
-			MessID = cur.insert_id()
-			cur.execute("""INSERT INTO Sensorknoten_Messwerte (KN_ID, MESS_ID)VALUES(%s, %s)""", (KN_ID, MessID))
-        con.close()
-
-
+    c, addr = s.accept()
+    print('Got connection from ', addr)
+    data = json.loads(c.recv(4096).encode('utf-8'))
+    Name = str(data["Name"])
+    SEN_ID = str(data["SEN_ID"])
+    Wert = str(data["Messwert"])
+    adresse = str(addr[0])
+    try:
+        con = mdb.connect('localhost', 'root', 'Piroot', 'Sicherheitssystem')
+        print("SQL-Connection successful")
+    except _mysql.Error, e:
+        print("SQL-Connection failed. Error %d: %s" % (e.args[0], e.args[1]))
+    with con:
+        cur = con.cursor()
+        row_count = cur.execute("SELECT KN_ID FROM Sensorknoten WHERE Knotennamen = %s", (Name,))
+        # Wenn ein Datenbankeintrag gefunden wird, wird die IPv4_Adresse erneuert
+        if row_count == 1:
+            KN_ID = cur.fetchone()
+            cur.execute("""UPDATE Sensorknoten SET IPv4_Adresse = %s WHERE Knotennamen = %s""", (adresse, Name))
+            cur.execute("""INSERT INTO Messwerte (SEN_ID, Messwert)VALUES(%s, %s)""", (SEN_ID, Wert))
+            # Die zuletzt erstellte ID wird übergeben
+            MessID = cur.insert_id()
+            cur.execute("""INSERT INTO Sensorknoten_Messwerte (KN_ID, MESS_ID)VALUES(%s, %s)""", (KN_ID, MessID))
+        # Wenn kein Datenbankeintrag gefunden wird, wird ein neuer Datenbankeintrag erstellt
+        elif row_count == 0:
+            cur.execute("""INSERT INTO Sensorknoten (Knotennamen, IPv4_Adresse)VALUES(%s, %s)""", (Name, adresse))
+            # Die zuletzt erstellte ID wird übergeben
+            KN_ID = cur.insert_id()
+            cur.execute("""INSERT INTO Messwerte (SEN_ID, Messwert)VALUES(%s, %s)""", (SEN_ID, Wert))
+            # Die zuletzt erstellte ID wird übergeben
+            MessID = cur.insert_id()
+            cur.execute("""INSERT INTO Sensorknoten_Messwerte (KN_ID, MESS_ID)VALUES(%s, %s)""", (KN_ID, MessID))
+    con.close()
